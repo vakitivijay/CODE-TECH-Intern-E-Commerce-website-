f6ffa87081768e946699e7523a4d1a3978f89b93
vakitivijay <151705799+vakitivijay@users.noreply.github.com>
2024-06-21 19:54:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,279 @@
+const image = document.getElementById('image');
+
+image.addEventListener('click', () => {
+    image.classList.toggle('clicked');
+});
+
+
+
+
+/* shirt function */
+
+let shirtimage = document.getElementById("shirtimage");
+let shirtname = document.getElementById("shirtname");
+let shirtdescription = document.getElementById("shirtdescription");
+let shirtprice = document.getElementById("shirtprice");
+
+
+
+function shirtfunction1() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20230605/jSRq/647de81cd55b7d0c634d0355/-473Wx593H-466239596-pink-MODEL.jpg"
+    shirtname.textContent = "THE BEAR HOUSE"
+    shirtdescription.textContent = "Checked Slim Fit Shirt with Patch Pocket"
+    shirtprice.textContent = "₹1,248";
+    
+
+}
+
+function shirtfunction2() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20230802/7ixt/64ca4c56a9b42d15c985425c/-473Wx593H-443016514-white-MODEL.jpg"
+    shirtname.textContent = "DNMX"
+    shirtdescription.textContent = "Men Checked Slim Fit Cotton Shirt"
+    shirtprice.textContent = "₹580"
+
+}
+
+function shirtfunction3() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240425/68Zb/662a5a4705ac7d77bb2a8f61/-473Wx593H-466866606-white-MODEL.jpg"
+    shirtname.textContent = "TISTABENE"
+    shirtdescription.textContent = "Spread Collar Shirt with Short Sleeves"
+    shirtprice.textContent = "₹539"
+
+}
+
+function shirtfunction4() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/h91/ha1/15812049076254/-473Wx593H-441015307-purple-MODEL.jpg"
+    shirtname.textContent = "NETWORK"
+    shirtdescription.textContent = "Shirt with Patch Pocketr"
+    shirtprice.textContent = "₹427"
+
+}
+
+function shirtfunction5() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240326/OJgp/6602a22416fd2c6e6a807765/-473Wx593H-466607648-black-MODEL.jpg"
+    shirtname.textContent = "SNITCH"
+    shirtdescription.textContent = "Printed Oversized Fit Shirt"
+    shirtprice.textContent = "₹799"
+
+}
+
+function shirtfunction6() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20230621/EPeU/64924a66d55b7d0c63889027/-473Wx593H-463775643-blue-MODEL.jpg"
+    shirtname.textContent = "CAMPUS SUTRA"
+    shirtdescription.textContent = "Checked Shirt with Patch Pocket"
+    shirtprice.textContent = "₹720"
+
+}
+
+function shirtfunction7() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20220824/xAzq/6305532baeb269176197eebc/-473Wx593H-441314815-ltblue-MODEL.jpg"
+    shirtname.textContent = "CAMPUS SUTRA"
+    shirtdescription.textContent = "Checked Shirt with Patch Pocket"
+    shirtprice.textContent = "₹720"
+
+}
+
+function shirtfunction8() {
+    shirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20230817/jgVE/64dd36bfa9b42d15c9b3fd22/-473Wx593H-466467749-red-MODEL.jpg"
+    shirtname.textContent = "YOUSTA"
+    shirtdescription.textContent = "Men Checked Regular Fit Shirt"
+    shirtprice.textContent = "₹499"
+
+}
+
+/* sherwani function */
+
+let sherwaniimage = document.getElementById("shirtimage");
+let sherwaniname = document.getElementById("shirtname");
+let sherwanidescription = document.getElementById("shirtdescription");
+let sherwaniprice = document.getElementById("shirtprice");
+
+function sherwanifunction1() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240502/OmLE/6633c03b16fd2c6e6ae39633/-473Wx593H-467296335-black-MODEL5.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Embroidered Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,400"
+
+}
+
+function sherwanifunction2() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240309/c09m/65eb8ff416fd2c6e6a489ff3/-473Wx593H-467149124-green-MODEL.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Floral Woven Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,890"
+
+}
+
+function sherwanifunction3() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240311/GPnB/65eede7505ac7d77bba727bf/-473Wx593H-467151964-black-MODEL.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Floral Woven Regular Fit Sherwani5,650"
+    sherwaniprice.textContent = "₹5,650"
+
+}
+
+function sherwanifunction4() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240311/8dMg/65eef18305ac7d77bba838dc/-473Wx593H-467151905-green-MODEL.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Geometric Woven Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹4,794"
+
+}
+
+function sherwanifunction5() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240308/FBXu/65eb143116fd2c6e6a46dd0f/-473Wx593H-467148191-white-MODEL.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Floral Woven Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,099"
+
+}
+
+function sherwanifunction6() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240502/uXJV/6633973d16fd2c6e6ae2806f/-473Wx593H-467296321-green-MODEL.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Embroidered Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,600"
+
+}
+
+function sherwanifunction7() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240308/lpF5/65eb146305ac7d77bba1f507/-473Wx593H-467148350-blue-MODEL3.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Floral Print Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,440"
+
+}
+
+function sherwanifunction8() {
+    sherwaniimage.src = "https://assets.ajio.com/medias/sys_master/root/20240425/saKU/6629657105ac7d77bb26b2b4/-473Wx593H-467276794-grey-MODEL5.jpg"
+    sherwaniname.textContent = "KISAH"
+    sherwanidescription.textContent = "Men Patterned Regular Fit Sherwani"
+    sherwaniprice.textContent = "₹5,670"
+
+}
+
+/* t-shirt function section */
+
+let tshirtimage = document.getElementById("shirtimage");
+let tshirtname = document.getElementById("shirtname");
+let tshirtdescription = document.getElementById("shirtdescription");
+let tshirtprice = document.getElementById("shirtprice");
+
+function tshirtfunction1() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20231130/RcvG/6568415bddf7791519a669f7/-473Wx593H-466722440-black-MODEL.jpg"
+    tshirtname.textContent = "DIFFERENCE OF OPINION"
+    tshirtdescription.textContent = "Graphic Oversized Fit T-shirt"
+    tshirtprice.textContent = "₹512"
+}
+
+function tshirtfunction2() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240329/s0G5/6606c2a216fd2c6e6a8aa041/-473Wx593H-467212229-black-MODEL.jpg"
+    tshirtname.textContent = "DILLINGER"
+    tshirtdescription.textContent = "Men Graphic Print Oversized Fit Round-Neck T-Shirt"
+    tshirtprice.textContent = "₹546"
+}
+
+function tshirtfunction3() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240503/HGC0/6634158705ac7d77bb3ad75c/-473Wx593H-467297285-maroon-MODEL.jpg"
+    tshirtname.textContent = "FORCE"
+    tshirtdescription.textContent = "Men Striped Oversized Crew-Neck T-Shirt"
+    tshirtprice.textContent = "₹375"
+}
+
+function tshirtfunction4() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240530/MqBc/6657d53716fd2c6e6a3c8b49/-473Wx593H-700024023-orange-MODEL.jpg"
+    tshirtname.textContent = "LEOTUDE"
+    tshirtdescription.textContent = "Men Typographic Print Oversized Fit T-Shirt"
+    tshirtprice.textContent = "₹449"
+}
+
+function tshirtfunction5() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240302/ryWE/65e2f86905ac7d77bb91eac7/-473Wx593H-467011186-white-MODEL.jpg"
+    tshirtname.textContent = "WEEZY"
+    tshirtdescription.textContent = "Men Printed Oversized Fit Crew-Neck T-Shirt"
+    tshirtprice.textContent = "₹888"
+}
+
+function tshirtfunction6() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240422/RE8I/6626674c05ac7d77bb1cdf29/-473Wx593H-467269833-white-MODEL.jpg"
+    tshirtname.textContent = "MISCHIEF MONKEY"
+    tshirtdescription.textContent = "Men Typographic Print Oversized Fit T-Shirt"
+    tshirtprice.textContent = "₹599"
+}
+
+function tshirtfunction7() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240415/aPa9/661d556616fd2c6e6ab23a9b/-473Wx593H-467251950-red-MODEL.jpg"
+    tshirtname.textContent = "GO DEVIL"
+    tshirtdescription.textContent = "Men Dice Print Oversized Crew-Neck T-Shirt"
+    tshirtprice.textContent = "₹860"
+}
+
+function tshirtfunction8() {
+    tshirtimage.src = "https://assets.ajio.com/medias/sys_master/root/20240410/TncP/66167ba305ac7d77bb0145b4/-473Wx593H-442592305-mustard-MODEL.jpg"
+    tshirtname.textContent = "YOUSTA"
+    tshirtdescription.textContent = "Men Printed Oversized Fit Crew-Neck T-Shirt"
+    tshirtprice.textContent = "₹399"
+}
+
+/* jogger function section */
+
+let joggerimage = document.getElementById("shirtimage");
+let joggername = document.getElementById("shirtname");
+let joggerdescription = document.getElementById("shirtdescription");
+let joggerprice = document.getElementById("shirtprice");
+
+function joggerfunction1() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240501/Absp/6632869316fd2c6e6adfcedd/-473Wx593H-467291293-blue-MODEL.jpg"
+    joggername.textContent = "JUMP CUTS"
+    joggerdescription.textContent = "Men Joggers with Flap Pockets"
+    joggerprice.textContent = "₹598"
+}
+
+function joggerfunction2() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20230628/PXyn/649b7465eebac147fc150a09/-473Wx593H-465762411-grey-MODEL.jpg"
+    joggername.textContent = "STYLI"
+    joggerdescription.textContent = "Men Mid-Rise Joggers with Slip Pockets"
+    joggerprice.textContent = "₹944"
+}
+
+function joggerfunction3() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240524/iuYv/6650d0a505ac7d77bb795f96/-473Wx593H-443029082-grey-MODEL.jpg"
+    joggername.textContent = "DNMX"
+    joggerdescription.textContent = "Men Regular Fit Parachute Pants"
+    joggerprice.textContent = "₹1,499"
+}
+
+function joggerfunction4() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240117/zpFA/65a7ed7416fd2c6e6aad58eb/-473Wx593H-466764020-green-MODEL.jpg"
+    joggername.textContent = "JUMP CUTS"
+    joggerdescription.textContent = "Men Joggers with Flap Pockets"
+    joggerprice.textContent = "₹486"
+}
+
+function joggerfunction5() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240516/o1So/6645f04205ac7d77bb619a3e/-473Wx593H-466267014-blue-MODEL.jpg"
+    joggername.textContent = "JUMP CUTS"
+    joggerdescription.textContent = "Joggers with Drawstring Waist"
+    joggerprice.textContent = "₹598"
+}
+
+function joggerfunction6() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240513/MILv/6641aa5616fd2c6e6a00455b/-473Wx593H-467327526-black-MODEL.jpg"
+    joggername.textContent = "PUMA"
+    joggerdescription.textContent = "Logo Print Joggers with Insert Pockets"
+    joggerprice.textContent = "₹1,959"
+}
+
+function joggerfunction7() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240318/kQmD/65f871cc05ac7d77bbc2d156/-473Wx593H-443028929-darkblue-MODEL.jpg"
+    joggername.textContent = "ECKO UNLTD"
+    joggerdescription.textContent = "Men Relaxed Fit Joggers"
+    joggerprice.textContent = "₹1,784"
+}
+
+function joggerfunction8() {
+    joggerimage.src = "https://assets.ajio.com/medias/sys_master/root/20240314/WiLk/65f3219c05ac7d77bbb808b5/-473Wx593H-442470634-black-MODEL.jpg"
+    joggername.textContent = "YOUSTA"
+    joggerdescription.textContent = "Men Graphic Print Regular Fit Joggers"
+    joggerprice.textContent = "₹699"
+}
